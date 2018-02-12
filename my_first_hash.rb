588304f134a1320ea2b41f202ff2ae64c34d6295
Trac Nguyen <nguyendtrac@gmail.com>
2018-02-12 01:32:00 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-  my_hash = { my_dogs_breed => "American Pit Bull Terrier"}
+  my_hash = { "my_dogs_breed" => "American Pit Bull Terrier"}
 
 end
 
